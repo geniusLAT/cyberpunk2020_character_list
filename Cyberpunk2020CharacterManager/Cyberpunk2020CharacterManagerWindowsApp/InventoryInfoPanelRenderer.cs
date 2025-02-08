83e255c3cd5243d3ef66938d03194419918b6028
geniuslat <traber73@yandex.ru>
2025-02-08 18:24:52 +0500

    Ability to ChooseEquipment

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020CharacterManagerWindowsApp/InventoryInfoPanelRenderer.cs
@@ -0,0 +1,11 @@
+﻿using Cyberpunk2020GameEntities;
+using Cyberpunk2020GameEntities.Cybernetics;
+using Cyberpunk2020GameEntities.Equipments;
+using System.Data.Common;
+
+namespace Cyberpunk2020CharacterManagerWindowsApp;
+
+public partial class Form1 : Form
+{
+    private Equipment _chosenEquipment;
+}
--- Cyberpunk2020CharacterManager/Cyberpunk2020CharacterManagerWindowsApp/InventoryRenderer.cs
+++ Cyberpunk2020CharacterManager/Cyberpunk2020CharacterManagerWindowsApp/InventoryRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using Cyberpunk2020GameEntities;
 using Cyberpunk2020GameEntities.Cybernetics;
+using Cyberpunk2020GameEntities.Equipments;
 using System.Data.Common;
 
 namespace Cyberpunk2020CharacterManagerWindowsApp;
@@ -7,8 +8,37 @@ namespace Cyberpunk2020CharacterManagerWindowsApp;
 public partial class Form1 : Form
 {
     int pixelsForOneEquipmentPanel = 14;
+    Label _clickedInventoryLabel;
 
-    Control RenderEquipmentPanel(string text, int i, int column, bool header, int s, int l)
+    private void EquipmentLabel_Click(object sender, EventArgs e)
+    {
+        var control = (Control)sender;
+        try
+        {
+            int index = int.Parse(control.Name);
+            ChooseEquipment(_chosenCharacter.equipments[index]);
+        }
+        catch (Exception)
+        {
+            return;
+        }
+
+        if (_clickedInventoryLabel != null)
+        {
+            ((Panel)_clickedInventoryLabel.Parent).BackColor = Color.White;
+            _clickedInventoryLabel.ForeColor = Color.Black;
+        }
+        ((Panel)control.Parent).BackColor = Color.Blue;
+        _clickedInventoryLabel = (Label)control;
+        _clickedInventoryLabel.ForeColor = Color.White;
+    }
+
+    private void ChooseEquipment(Equipment equipment)
+    {
+        _chosenEquipment = equipment;
+    }
+
+    Control RenderEquipmentPanel(string text, int i, int column, int s, int l)
     {
         int text_size = 180;
         int extra_size = 20;
@@ -17,44 +47,16 @@ public partial class Form1 : Form
         _inventoryPanels.Add(equipmentPanel);
         inventoryScrollPanel.Controls.Add(equipmentPanel);
         Label that_label = new Label();
+        that_label.Name = i.ToString();
+        that_label.Click += new EventHandler(EquipmentLabel_Click);
         that_label.Size = new Size(text_size, pixelsForOneEquipmentPanel);
+        that_label.Text = text;
 
         equipmentPanel.Controls.Add(that_label);
         equipmentPanel.Size = new Size(extra_size + text_size, pixelsForOneEquipmentPanel);
-        //skill_panel.BackColor = Color.Yellow;
-        that_label.Text = text + "____________________________________";// "skill" + i.ToString();
-
-        if (IsProfessionalSkill(text))
-        {
-            that_label.ForeColor = Color.Blue;
-        }
-
-        //that_label.Text =  "skill" + i.ToString();
+       
         equipmentPanel.Location = new Point((extra_size + text_size) * column, i * pixelsForOneEquipmentPanel);
-
-
-        if (header)
-        {
-
-
-            that_label.Font = new Font(that_label.Font, that_label.Font.Style | FontStyle.Bold);
-
-        }
-        else
-        {
-            NumericUpDown skillNumeric = new NumericUpDown();
-            //skillNumeric.Enabled= false;
-            equipmentPanel.Controls.Add(skillNumeric);
-            skillNumeric.Location = new Point(l, 0);
-            //that_label.Text = skillNumeric.Size.Width.ToString()+", "+ skillNumeric.Size.Height.ToString();
-            skillNumeric.Size = new Size(s, 20);
-            skillNumeric.Value = 0;
-            skillNumeric.Minimum = 0;
-            skillNumeric.Maximum = 10;
-            skillNumeric.ValueChanged += SkillPointChanged;
-        }
         return equipmentPanel;
-
     }
 
     void RenderInventory(int s, int l)
@@ -69,7 +71,7 @@ public partial class Form1 : Form
         foreach (var item in _chosenCharacter.equipments)
         {
 
-            RenderEquipmentPanel(item.Name, n++, c, false, s, l);
+            RenderEquipmentPanel(item.Name, n++, c, s, l);
         }
     }
 }
