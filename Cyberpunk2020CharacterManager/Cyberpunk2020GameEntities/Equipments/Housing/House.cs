2ee1f0f5a76f8412b1b6966f14302956ec074071
geniuslat <traber73@yandex.ru>
2025-02-09 12:11:30 +0500

    Housing added

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Housing/House.cs
@@ -0,0 +1,14 @@
+﻿namespace Cyberpunk2020GameEntities.Equipments.Housing;
+
+public class House : Housing
+{
+    public override string Name { get { return "Дом (за комнату в месяц)"; } }
+
+    public override int BookIndex { get; set; } = 3;
+
+    public House()
+    {
+        Description = string.Empty;
+        Cost = 150;
+    }
+}
---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Housing/Utilities.cs
@@ -0,0 +1,14 @@
+﻿namespace Cyberpunk2020GameEntities.Equipments.Housing;
+
+public class Utilities : Equipment
+{
+    public override string Name { get { return "Коммунальные услуги (в месяц)"; } }
+
+    public override int BookIndex { get; set; } = 4;
+
+    public Utilities()
+    {
+        Description = string.Empty;
+        Cost = 100;
+    }
+}
