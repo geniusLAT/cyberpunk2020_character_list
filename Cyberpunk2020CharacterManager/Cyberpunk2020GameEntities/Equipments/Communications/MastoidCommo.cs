bdc65b6ff49821939e2f0e2ffa86bb93243ef25e
geniuslat <traber73@yandex.ru>
2025-02-09 10:56:13 +0500

    Communications BookIndex sorting

--- Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Communications/MastoidCommo.cs
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Communications/MastoidCommo.cs
@@ -4,6 +4,8 @@ public  class MastoidCommo : Equipment
 {
     public override string Name { get { return "Мастоид-Коммуникатор"; } }
 
+    public override int BookIndex { get; set; } = 0;
+
     public MastoidCommo()
     {
         Description = " Все коммуникаторы являются радиопередатчиками. Этот приклеенный к " +
--- Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Communications/MiniCellPhone.cs
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Communications/MiniCellPhone.cs
@@ -4,6 +4,8 @@ public  class MiniCellPhone : Equipment
 {
     public override string Name { get { return "Мини Сотовый Телефон"; } }
 
+    public override int BookIndex { get; set; } = 4;
+
     public MiniCellPhone()
     {
         Description = " Помещается в пачку сигарет";
