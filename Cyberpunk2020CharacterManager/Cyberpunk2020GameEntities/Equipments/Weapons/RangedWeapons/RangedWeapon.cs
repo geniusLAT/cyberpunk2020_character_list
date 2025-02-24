cb2387a9026674616d34df09cd6ece968abbd205
geniuslat <traber73@yandex.ru>
2025-02-24 19:52:56 +0500

    SmartWeaponAdded

--- Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Weapons/RangedWeapons/RangedWeapon.cs
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Weapons/RangedWeapons/RangedWeapon.cs
@@ -2,5 +2,45 @@
 
 public abstract class RangedWeapon : Weapon
 {
+    string lastOptionChosen = string.Empty;
 
+    static Dictionary<string, int> Options = new Dictionary<string, int>()
+    {
+        {"Стандарт (1х цена)",1 },
+        {"Умное (2х цена)", 2}
+    };
+
+    public override List<string> GetOptions()
+    {
+        if(type == WeaponType.Heavy)
+        {
+            return new List<string>();
+        }
+
+        var result = new List<string>();
+        foreach (var item in Options)
+        {
+            result.Add(item.Key);
+        }
+        return result;
+    }
+
+    public override int GetOptionPriceModifier(string option)
+    {
+        var PriceModifier = 1;
+        Options.TryGetValue(option, out PriceModifier);
+        return PriceModifier;
+    }
+
+    public override void ChangeOption(string newOption)
+    {
+        lastOptionChosen = newOption;
+    }
+
+    public override void Add(Character character, Random random)
+    {
+        Detail += lastOptionChosen;
+        Cost *= GetOptionPriceModifier(lastOptionChosen);
+        base.Add(character, random);
+    }
 }
