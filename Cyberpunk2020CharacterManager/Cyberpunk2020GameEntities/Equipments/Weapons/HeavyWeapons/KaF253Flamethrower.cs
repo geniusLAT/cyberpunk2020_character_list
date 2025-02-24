1c273ef86d56eadaabeec7fe2604bd964c9b496b
geniuslat <traber73@yandex.ru>
2025-02-24 23:17:22 +0500

    Rest of heavy weapons added

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Weapons/HeavyWeapons/KaF253Flamethrower.cs
@@ -0,0 +1,31 @@
+﻿namespace Cyberpunk2020GameEntities.Equipments.Weapons.HeavyWeapons;
+
+public class KaF253Flamethrower : Weapon
+{
+    public override string Name { get { return "K-A F-253 Flamethrower"; } }
+
+    public override int BookIndex { get; set; } = 6;
+
+    public override int GenerateDamage(Random random)
+    {
+        return random.Next(1, 11) + random.Next(1, 11);
+    }
+
+    public KaF253Flamethrower()
+    {
+        Description = "Распылитель жидкого напалма. Наспинный и громоздкий." +
+            " Наносит дополнительный урон после первого попадания (см. ПвПВ, стр. 110).";
+
+        type = WeaponType.Heavy;
+        WeaponAccuracy = -2;
+        Concealability = WeaponConcealability.CantBeHidden;
+        Avaliability = WeaponAvaliability.Rare;
+        DamageFormula = "2D10+";
+        AmmoType = WeaponAmmoType.Bullet20mm;
+        shots = 10;
+        RateOfFireInTurn = 1;
+        Reliability = WeaponReliability.VeryReliable;
+        Range = 50;
+        Cost = 1500;
+    }
+}
---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Equipments/Weapons/HeavyWeapons/Mine.cs
@@ -0,0 +1,30 @@
+﻿namespace Cyberpunk2020GameEntities.Equipments.Weapons.HeavyWeapons;
+
+public class Mine : Weapon
+{
+    public override string Name { get { return "Мина"; } }
+
+    public override int BookIndex { get; set; } = 4;
+
+    public override int GenerateDamage(Random random)
+    {
+        return random.Next(1, 11) + random.Next(1, 11) + random.Next(1, 11) + random.Next(1, 11);
+    }
+
+    public Mine()
+    {
+        Description = "Может быть взорвана таймером, натяжением растяжки, сигналом или датчиком движения.";
+
+        type = WeaponType.Heavy;
+        WeaponAccuracy = 0;
+        Concealability = WeaponConcealability.Jacket;
+        Avaliability = WeaponAvaliability.Poor;
+        DamageFormula = "4D10";
+        AmmoType = WeaponAmmoType.Bullet20mm;
+        shots = 10;
+        RateOfFireInTurn = 1;
+        Reliability = WeaponReliability.VeryReliable;
+        Range = 0;
+        Cost = 350;
+    }
+}
