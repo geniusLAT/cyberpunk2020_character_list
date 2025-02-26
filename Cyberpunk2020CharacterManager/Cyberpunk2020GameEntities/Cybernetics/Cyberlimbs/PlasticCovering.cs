7f43fa4663a676e4a49c90d990fd5c006ac9b938
geniuslat <traber73@yandex.ru>
2025-02-27 00:09:39 +0500

    Cyberlimb covering

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/Cyberlimbs/PlasticCovering.cs
@@ -0,0 +1,21 @@
+﻿namespace Cyberpunk2020GameEntities.Cybernetics.Cyberlimbs;
+
+public class PlasticCovering : CyberlimbCovering
+{
+    public override string Name { get { return "Пластиковое покрытие"; } }
+
+    public PlasticCovering()
+    {
+        SurgeryCode = SurgeryCode.Negligible;
+        Description = " когда любая кибер-конечность находится в оголённом или непокры- том состоянии, её " +
+            "можно покрыть различными способами. Самый дешёвый метод это пластиковое покрытие," +
+            " доступное во множестве расцветок, аэрографией или прозрачное, встроенной подсветкой и голографией.";
+        HumanityLossFormula = "-";
+        Cost = 200; //TODO Max Cost
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = - random.Next(1, 7) / 2;
+    }
+}
---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/Cyberlimbs/RealSkin.cs
@@ -0,0 +1,22 @@
+﻿namespace Cyberpunk2020GameEntities.Cybernetics.Cyberlimbs;
+
+public class RealSkin : CyberlimbCovering
+{
+    public override string Name { get { return "Покрытие RealSkin"; } }
+
+    public RealSkin()
+    {
+        SurgeryCode = SurgeryCode.Negligible;
+        Description = " Самый дорогой вариант - Realskinn, гибкий пластик, очень похожий на кожу," +
+            " с фолли- кулами, волосками, небольшими шрамами " +
+            "и дефектами, он имеет 75 % шанс сойти за \"мясную\" конечность " +
+            "для всех, кроме самых внимательных наблюдателей.";
+        HumanityLossFormula = "-";
+        Cost = 200;
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = - random.Next(1, 7) / 2;
+    }
+}
