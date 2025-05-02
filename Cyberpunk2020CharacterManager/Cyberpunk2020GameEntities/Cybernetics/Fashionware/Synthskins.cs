2ebb7ba5012e4a4b6c064008d9265e93ff3f666b
geniuslat <traber73@yandex.ru>
2025-05-02 16:24:46 +0500

    Other fashionwares

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/Fashionware/Synthskins.cs
@@ -0,0 +1,27 @@
+﻿namespace Cyberpunk2020GameEntities.Cybernetics.Fashionware;
+
+public class Synthskins : Implant
+{
+    public override string Name { get { return "Синт-Кожа"; } }
+
+    public Synthskins()
+    {
+        SurgeryCode = SurgeryCode.Negligible;
+        Description = " более сложная версия технологии светящейся татуировки, искусственная кожа - это слой изменяющего цвет пластика, " +
+            "связанный с внешней кожей персонажа. Она может быть настроена для отображения цветов, рисунков, " +
+            "вспышек света или других специальных эффектов, используя микросхемы настройки (стоимость 100 eb) " +
+            "которые вставляются в разъём на коже (обычно под линией волос).";
+        HumanityLossFormula = "1D6";
+        Cost = 400;
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = random.Next(1, 7);
+    }
+
+    public override string BarriersForChipIn(Character character)
+    {
+        return UniquenessPotentialProblem(character);
+    }
+}
---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/Fashionware/Techhair.cs
@@ -0,0 +1,30 @@
+﻿namespace Cyberpunk2020GameEntities.Cybernetics.Fashionware;
+
+public class Techhair : Implant
+{
+    public override string Name { get { return "Тех-Волосы"; } }
+
+    public Techhair()
+    {
+        SurgeryCode = SurgeryCode.Minor;
+        Description = "Стержни этих искусственных волос пропитаны различными типами реактивных химикатов. " +
+            "Некоторые типы чувствительны к температуре и меняют цвет или поднимаются в зависимости от погоды." +
+            " Другие содержат те же пигменты, которые используются в светящихся татуировках, накапливая и" +
+            " испуская свет в цветных узорах. Третьи могут менять цвет по твоему желанию, используя" +
+            " специальные химические шампуни. Тех-Волосы могут быть имплантированы в ирокезы, в кудри," +
+            " парики, гривы, косы, усы и другие менее очевидные (но интересные) места.";
+        HumanityLossFormula = "2";
+        Cost = 1;
+        MaxCost = 200;
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = 2;
+    }
+
+    public override string BarriersForChipIn(Character character)
+    {
+        return string.Empty;
+    }
+}
