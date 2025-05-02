3662d59c5c862c0b76ec6144164ac4b53f66e652
geniuslat <traber73@yandex.ru>
2025-05-02 16:03:26 +0500

    Biomonitor.cs added

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/Fashionware/Biomonitor.cs
@@ -0,0 +1,32 @@
+﻿namespace Cyberpunk2020GameEntities.Cybernetics.Fashionware;
+
+public class Biomonitor : Implant
+{
+    public override string Name { get { return "Биомонитор"; } }
+
+    public Biomonitor()
+    {
+        SurgeryCode = SurgeryCode.Minor;
+        Description = " это фаворит Соло, фанатов гаджетов и взволнованных Корпоратов," +
+            " обеспокоенных их кровяным давление. Установленный под кожу предплечья " +
+            "Биомонитор постоянно замеряет показатели сердцебиения, дыхания, мозговых " +
+            "волн, уровня сахара в крови, температуры и уровня холестерина. Дисплей " +
+            "представляет собой узор из светодиодов в форме слов каждый из которых имеет" +
+            " цветовую последовательность от красного (критический) до зелёного (отличный)." +
+            " По мере изменения показаний цвет меняется. Пользователь просто откидывает" +
+            " назад свою манжету, находит нужное ему светящееся слово и смотрит на цвет. " +
+            "С точки зрения игры, это добавляет +2 к любой проверке Сопротивлениz пыткам/ наркотикам.";
+        HumanityLossFormula = "1";
+        Cost = 100;
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = 1;
+    }
+
+    public override string BarriersForChipIn(Character character)
+    {
+        return string.Empty;
+    }
+}
