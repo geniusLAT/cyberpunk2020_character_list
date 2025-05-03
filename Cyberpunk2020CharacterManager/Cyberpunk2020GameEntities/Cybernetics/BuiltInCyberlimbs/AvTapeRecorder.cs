905922b85bc9bba7d2db2509c018678c76ff6b08
geniuslat <traber73@yandex.ru>
2025-05-03 17:13:00 +0500

    BuiltInCyberlimb

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020GameEntities/Cybernetics/BuiltInCyberlimbs/AvTapeRecorder.cs
@@ -0,0 +1,22 @@
+﻿using Cyberpunk2020GameEntities.Cybernetics.Neuralwares;
+
+namespace Cyberpunk2020GameEntities.Cybernetics.BuiltInCyberlimbs;
+
+public class AvTapeRecorder : BuiltInCyberlimb
+{
+    public override string Name { get { return "Аудио / видео магнитофон"; } }
+
+    public AvTapeRecorder()
+    {
+        SurgeryCode = SurgeryCode.Negligible;
+        Description = "Это устройство использует микрокассеты для хранения входных данных со своего внутреннего микрофона," +
+            " видеокамеры или цифрового канала записи.";
+        HumanityLossFormula = "1";
+        Cost = 250;
+    }
+
+    public override void GenerateHumanLoss(Random random)
+    {
+        HumanityLoss = 1;
+    }
+}
