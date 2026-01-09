d4b8cd1469a7009a7d8dd4eb91e19cb643426e59
geniuslat <traber73@yandex.ru>
2026-01-09 17:27:25 +0500

    project added

---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020CharacterManagerServer/Controllers/StabilityCheckController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cyberpunk2020CharacterManagerServer.Controllers;
+
+[ApiController]
+[Route("/stability")]
+public class StabilityCheckController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<StabilityCheckController> _logger;
+
+    public StabilityCheckController(ILogger<StabilityCheckController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<StabilityResponse> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new StabilityResponse
+        {
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ Cyberpunk2020CharacterManager/Cyberpunk2020CharacterManagerServer/StabilityResponse.cs
@@ -0,0 +1,7 @@
+namespace Cyberpunk2020CharacterManagerServer
+{
+    public class StabilityResponse
+    {
+      public string? Summary { get; set; }
+    }
+}
